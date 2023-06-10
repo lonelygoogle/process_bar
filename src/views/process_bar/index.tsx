fbe9087ed13abc25f205353d2491f0fdb44a8852
stevenHuang1010 <steven.huang@cafx.com>
2023-06-10 21:45:45 +1000

    finish function

--- src/views/process_bar/index.tsx
+++ src/views/process_bar/index.tsx
@@ -1,22 +1,65 @@
 import { useState } from "react";
 import "./index.scss";
+import { buttonsValue, optionsValue } from "./schema";
 
-const buttonValue = ["-25", "-10", "+10", "+25"];
-const ProcessBar = () => {
-  const [width, setWidth] = useState(0);
+const ProcessBar: React.FC = () => {
+  const [progressBars, setProgressBars] = useState<number[]>([0, 0, 0]);
+  const [selectedLine, setSelectedLine] = useState<number>(0);
 
-  const increaseWidth = () => {
-    setWidth((prevWidth) => prevWidth + 10);
+  const handleProgressBarChange = (index: number, amount: number) => {
+    setProgressBars((prevProgressBars) => {
+      const updatedProgressBars = [...prevProgressBars];
+      updatedProgressBars[index] += amount;
+      updatedProgressBars[index] = Math.max(0, updatedProgressBars[index]);
+      return updatedProgressBars;
+    });
+  };
+
+  const handleLineSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedLine(parseInt(event.target.value));
+  };
+
+  const handleButtonClick = (amount: number) => {
+    handleProgressBarChange(selectedLine, amount);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="progress-container">
-          <div className="progress-bar" style={{ width: `${width}%` }}></div>
-          <button onClick={increaseWidth}>+10</button>
+      <div className="App-container">
+        {progressBars.map((value, index) => (
+          <div key={index} className="progress-container">
+            <p className="progress-value">{`${value}%`}</p>
+            <div
+              className={value > 100 ? "progress-bar red" : "progress-bar"}
+              style={{ width: `${Math.min(100, value)}%` }}
+            ></div>
+          </div>
+        ))}
+
+        <div className="control">
+          <select
+            className="selectLine"
+            value={selectedLine}
+            onChange={handleLineSelection}
+          >
+            {optionsValue.map((option) => (
+              <option key={option.key} value={option.key}>
+                {option.value}
+              </option>
+            ))}
+          </select>
+          <div className="buttons">
+            {buttonsValue.map((button) => (
+              <button
+                key={button.key}
+                onClick={() => handleButtonClick(Number(button.value))}
+              >
+                {button.value}
+              </button>
+            ))}
+          </div>
         </div>
-      </header>
+      </div>
     </div>
   );
 };
---
+++ src/views/process_bar/schema.ts
@@ -0,0 +1,38 @@
+export interface Values {
+  key: number;
+  value: string;
+}
+
+export const buttonsValue: Values[] = [
+  {
+    key: 0,
+    value: "-25",
+  },
+  {
+    key: 1,
+    value: "-10",
+  },
+  {
+    key: 2,
+    value: "+10",
+  },
+  {
+    key: 3,
+    value: "+25",
+  },
+];
+
+export const optionsValue: Values[] = [
+  {
+    key: 0,
+    value: "#process1",
+  },
+  {
+    key: 1,
+    value: "#process2",
+  },
+  {
+    key: 2,
+    value: "#process3",
+  },
+];
