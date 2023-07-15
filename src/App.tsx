892440ec2c245b97b03b5661bce7c4d3e2e22e76
steven huang <steven.huang@highlimitstudio.com>
2023-07-15 23:44:19 +1000

    memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import React, { lazy, Suspense } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
-const LazyProcessBar = lazy(() => import('./views/process_bar'));
-
-const App: React.FC = () => {
+const Child: React.FC<{ title: any; onChange: any }> = ({ title, onChange }) => {
+  console.log('Child render....');
   return (
-    <div className='App' data-testid='process-bar'>
-      <div className='App-container'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <LazyProcessBar />
-        </Suspense>
-      </div>
+    <div style={{ background: 'gray' }} onClick={() => onChange(100)}>
+      I am child: {title.subData}
     </div>
   );
 };
 
+const ChildMemo = React.memo(Child);
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const [subData, setSubData] = useState('haha');
+  console.log('Father render....');
+  const onChangeCount = useCallback(newCount => setCount(newCount), []);
+  return (
+    <div>
+      <h1>I am Parent: 被点了{count}次</h1>
+      {/* <ChildMemo title={subData} onChange={newCount => setCount(newCount)} /> */}
+      {/* <ChildMemo title={subData} onChange={onChangeCount} /> */}
+      <ChildMemo
+        title={useMemo(() => ({ name: subData, age: 1 }), [subData])}
+        onChange={onChangeCount}
+      />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+};
 export default App;
